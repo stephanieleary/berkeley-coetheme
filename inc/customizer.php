2d7f3af8680985188caf659e4d740bcdfb6854f6
Stephanie Leary <steph@sillybean.net>
2016-05-03 16:52:04 -0500

    adjust customizer instructions and section priority

--- inc/customizer.php
+++ inc/customizer.php
@@ -9,7 +9,7 @@ function berkeley_customizer_register() {
 	$image = apply_filters( 'berkeley_header_bg_image_default', '' );
 
 	$wp_customize->add_section( 'berkeley-settings', array(
-		'description' => __( 'You may upload a header background image here.<br /><br />The default image is <strong>1200 pixels wide and 235 pixels tall</strong>.<br /><br />To upload an image to replace your site title, see the Header Image section of the Customizer.', 'beng' ),
+		'description' => __( 'You may upload a header background image here.<br /><br />The default image is <strong>1200 pixels wide and 235 pixels tall</strong>.<br /><br />To upload an image to replace your site title instead, see the Header Image section of the Customizer.', 'beng' ),
 		'title'    => __( 'Header Background Image', 'beng' ),
 		'priority' => 35,
 	) );
@@ -23,5 +23,6 @@ function berkeley_customizer_register() {
 		'label' => __( 'Header Background Image', 'beng' ),
 		'section' => 'berkeley-settings',
 		'mime_type' => 'image',
+		'priority'	=> 1,
 	) ) );
 }
